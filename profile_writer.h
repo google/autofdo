e90cadf30144b582176873560a1b7f3cdc50fe21
Dhruv Chawla <dhruvc@nvidia.com>
2025-10-16 01:09:42 -0700

    Write full file path to GCOV if present

--- profile_writer.h
+++ profile_writer.h
@@ -204,7 +204,11 @@ class StringTableUpdater: public SymbolTraverser {
  protected:
   void Visit(const Symbol *node) override {
     if (node->info.func_name != nullptr)
-      file_map_->AddFileName(node->info.func_name, node->info.file_name);
+      file_map_->AddFileName(node->info.func_name,
+                             node->info.dir_name != ""
+                                 ? node->info.dir_name + "/" +
+                                       node->info.file_name
+                                 : node->info.file_name);
     for (const auto &pos_count : node->pos_counts) {
       for (const auto &name_count : pos_count.second.target_map) {
         (*map_)[name_count.first] = 0;
